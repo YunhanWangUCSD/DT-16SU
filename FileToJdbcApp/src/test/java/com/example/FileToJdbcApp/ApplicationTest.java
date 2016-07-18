f1d879494bde07849e646c599a38f6f790f29d2e
Yunhan Wang <yunhanwang@ucsd.edu>
2016-07-18 14:48:54 -0700

    add clean version of file to jdbc app and clean old versions

--- FileToJdbcApp/src/test/java/com/example/FileToJdbcApp/ApplicationTest.java
+++ FileToJdbcApp/src/test/java/com/example/FileToJdbcApp/ApplicationTest.java
@@ -3,6 +3,7 @@ package com.example.FileToJdbcApp;
 import com.datatorrent.api.LocalMode;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.validation.ConstraintViolationException;
@@ -13,23 +14,35 @@ import java.io.IOException;
  * The assumption to run this test case is that test_jdbc_table
  * and meta-table are created already.
  */
-public class ApplicationTest
-{
+public class ApplicationTest {
 
-    @Test
-    public void testApplication() throws IOException, Exception
-    {
-        try {
-            LocalMode lma = LocalMode.newInstance();
-            Configuration conf = new Configuration(false);
-            conf.addResource(this.getClass().getResourceAsStream("/META-INF/properties.xml"));
-            lma.prepareDAG(new FileToJdbcApp(), conf);
-            LocalMode.Controller lc = lma.getController();
-            lc.run(50000); // runs for 10 seconds and quits
-        } catch (ConstraintViolationException e) {
-            Assert.fail("constraint violations: " + e.getConstraintViolations());
-        }
+  @Test
+  @Ignore
+  public void testCsvParserApp() throws IOException, Exception {
+    try {
+      LocalMode lma = LocalMode.newInstance();
+      Configuration conf = new Configuration(false);
+      conf.addResource(this.getClass().getResourceAsStream("/META-INF/properties.xml"));
+      lma.prepareDAG(new FileToJdbcCsvParser(), conf);
+      LocalMode.Controller lc = lma.getController();
+      lc.run(50000); // runs for 10 seconds and quits
+    } catch (ConstraintViolationException e) {
+      Assert.fail("constraint violations: " + e.getConstraintViolations());
     }
+  }
 
+  @Test
+  @Ignore
+  public void testCustomParserApp() throws IOException, Exception {
+    try {
+      LocalMode lma = LocalMode.newInstance();
+      Configuration conf = new Configuration(false);
+      conf.addResource(this.getClass().getResourceAsStream("/META-INF/properties.xml"));
+      lma.prepareDAG(new FileToJdbcCustomParser(), conf);
+      LocalMode.Controller lc = lma.getController();
+      lc.run(50000); // runs for 10 seconds and quits
+    } catch (ConstraintViolationException e) {
+      Assert.fail("constraint violations: " + e.getConstraintViolations());
+    }
+  }
 }
-
--- MyFileToJdbcApp/README.md
+++
@@ -1,4 +0,0 @@
-## File to JDBC application with Customized Parse
-
-
-This example is very similar to the fileToJdbc example with one difference: it shows how to create custom parser to parse the input file.
